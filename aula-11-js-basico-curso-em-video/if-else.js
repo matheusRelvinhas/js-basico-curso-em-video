b373779e47f30c7be5e01e16729f8f9f2427325c
Matheus Relvinhas <matheusrelvinhas@gmail.com>
2022-08-18 21:34:32 -0300

    primeiroCommit

---
+++ aula-11-js-basico-curso-em-video/if-else.js
@@ -0,0 +1,25 @@
+console.log('O console funcionou corretamente.')
+
+console.log('_____________________________________')
+console.log(' ')
+/*condição-simples if*/
+
+let vel =  61
+console.log(vel)
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+if (vel > 60) {
+  console.log('Você ultrapassou a velocidade permitida, multado !')
+}
+console.log('Diriga sempre usando cinto de segurança.')
+
+console.log('_____________________________________')
+console.log(' ')
+/*condição-composta if/else*/
+
+let country = 'Brasil'
+console.log(`Vivendo em ${country}`)
+if (country === 'Brasil') {
+  console.log('Você é Brasileiro!')
+} else {
+  console.log('Você é Estrangeiro!')
+}
