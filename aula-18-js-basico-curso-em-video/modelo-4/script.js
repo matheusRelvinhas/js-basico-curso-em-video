13e9d04e1f1005eba549ff90102c0f8607634b85
Matheus Relvinhas <matheusrelvinhas@gmail.com>
2022-08-22 22:18:21 -0300

    tabuada

--- aula-18-js-basico-curso-em-video/modelo-4/script.js
+++ aula-18-js-basico-curso-em-video/modelo-4/script.js
@@ -1,1 +1,15 @@
-alert('Olá');
+function tabuada() {
+  let num = document.getElementById('num');
+  let tab = document.getElementById('res-tab')
+  if (num.value == 0) {
+    window.alert('Por favor digite um número diferente de 0.')
+  } else {
+    tab.innerHTML = ''
+    let n = Number(num.value)
+    for (i = 1; i <= 10; i++) {
+      let item = document.createElement('option')
+      item.text = `${n} x ${i} = ${n*i}`
+      tab.appendChild(item)
+    }
+  }
+}
