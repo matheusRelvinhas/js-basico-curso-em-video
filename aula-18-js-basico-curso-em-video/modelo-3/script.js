a31aae3717ba6d3da8e2194d442183301e0ab52c
Matheus Relvinhas <matheusrelvinhas@gmail.com>
2022-08-22 21:22:54 -0300

    pass-erro

--- aula-18-js-basico-curso-em-video/modelo-3/script.js
+++ aula-18-js-basico-curso-em-video/modelo-3/script.js
@@ -4,33 +4,37 @@ function contar() {
   let numPass =  document.getElementById('num-pass');
   let res = document.getElementById('res');
   if (numInit.value.length == 0 || numEnd.value.length == 0 || numPass.value.length == 0) {
-    window.alert('[ERRO] Faltam informações')
+    window.alert('[ERRO] Faltam informações.')
   } else {
     res.innerHTML = 'Contando: '
     let init = Number(numInit.value)
     let end = Number(numEnd.value)
     let pass = Number(numPass.value)
-    if (init < end) {
-      if (pass <= 0) {
-        window.alert('[ERROR] Não é possível calcular')
-      } else {
-        for (i = init; i <= end; i += pass) {
-          if (i == end || i > end - pass) {
-            res.innerHTML += `${i} \u{1F3C1}`
-          } else {
-            res.innerHTML += `${i} \u{1F449} `
+    if(pass == 0) {
+      window.alert('[ERROR] Passo igual a 0.')
+    } else {
+      if (init < end) {
+        if (pass < 0) {
+          window.alert('[ERROR] Não é possível calcular, passo menor que 0.')
+        } else {
+          for (i = init; i <= end; i += pass) {
+            if (i == end || i > end - pass) {
+              res.innerHTML += `${i} \u{1F3C1}`
+            } else {
+              res.innerHTML += `${i} \u{1F449} `
+            }
           }
         }
-      }
-    } else {
-      if (pass > 0) {
-        window.alert('[ERROR] Não é possível calcular')
       } else {
-        for (i = init; i >= end; i += pass) {
-          if (i == end || i < end - pass) {
-            res.innerHTML += `${i} \u{1F3C1}`
-          } else {
-            res.innerHTML += `${i} \u{1F449} `
+        if (pass > 0) {
+          window.alert('[ERROR] Não é possível calcular, passo maior que 0.')
+        } else {
+          for (i = init; i >= end; i += pass) {
+            if (i == end || i < end - pass) {
+              res.innerHTML += `${i} \u{1F3C1}`
+            } else {
+              res.innerHTML += `${i} \u{1F449} `
+            }
           }
         }
       }
